0faaca309f10b3700887f84886def96abf468555
Alexandra <alexandra.geier@ringieraxelspringer.ch>
2021-04-22 23:33:22 +0200

    ➕ add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {  
   return (
     <div className="App">
       <header className="App-header">
