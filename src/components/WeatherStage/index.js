51f0d6ca251723496c170cf1ba665098b3ddbbd4
Alexandra <alexandra.geier@ringieraxelspringer.ch>
2021-05-10 19:10:11 +0200

    🐛 catch API error

--- src/components/WeatherStage/index.js
+++ src/components/WeatherStage/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { WEATHER_API_7_DAYS, WEATHER_API_24_HOURS, WEATHER_API_CURRENT, fetchAccessToken, fetchWeather }  from '../../utils/weather';
+import { WEATHER_API_7_DAYS, WEATHER_API_CURRENT, fetchAccessToken, fetchWeather }  from '../../utils/weather';
 import WeatherCurrentDay from './../WeatherCurrentDay';
 import WeatherWeekDay from './../WeatherWeekDay';
 import './styles.css';
@@ -17,8 +17,12 @@ const WeatherStage = () => {
           const weatherCurrent = await fetchWeather(WEATHER_API_CURRENT, accessToken, 47.36667, 8.5 );
           const weather7Days = await fetchWeather(WEATHER_API_7_DAYS, accessToken, 47.36667, 8.5 );
           setIsLoaded(true);
-          setCurrentWeather(weatherCurrent);
-          setWeekWeather((weather7Days['7days'] && weather7Days['7days'].splice(1,7)) || []);
+          if (weatherCurrent){
+            setCurrentWeather(weatherCurrent);
+            setWeekWeather((weather7Days['7days'] && weather7Days['7days'].splice(1,7)) || []);
+          } else {
+            setError({message: 'Try again later.'});
+          }
         } catch (error) {
             setIsLoaded(true);
             setError(error);
